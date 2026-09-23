9101def80ea32a7d22c083c242f71123c93c9756
ajaykumarpandey8076-del <ajaykumarpandey8076@gmail.com>
2026-09-23 23:05:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 
+const GEMINI_API_KEY = 'AIzaSyA.AQ.Ab8RN6L7kky3HB0VcPQat64LIaKMBItdLX2iik0tr3ocGZ1Y9A';
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
