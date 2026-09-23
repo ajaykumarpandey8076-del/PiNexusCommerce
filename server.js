2b643b076969234317771505a7487dd6b40b6e78
ajaykumarpandey8076-del <ajaykumarpandey8076@gmail.com>
2026-09-23 22:07:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'OK' });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-module.exports = app;
-
 
